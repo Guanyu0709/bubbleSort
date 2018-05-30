ec694ae0ccd33540487f1b8573ad95c2cd8e346d
Guanyu0709 <esz880706@gmail.com>
2018-05-30 22:16:25 +0800

    指向常數資料的常數指標

---
+++ BSt4.c
@@ -0,0 +1,13 @@
+void BubbleSort(const int *const number,int *number2){
+    int i = 0, j = 0,temp=0;
+ for( i = 0; i < 10; i++) {
+        for( j = i; j < 10; j++) {
+            if( number2[j] < number2[i] ) {
+                temp = number[j];
+                number2[j] = number2[i];
+                number2[i] = temp;
+            }
+        }
+    }
+
+}
